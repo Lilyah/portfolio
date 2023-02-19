631af251f6e8208eb57b11051db186a23240f7fc
Lilqna Vankova <lilyah@Lilqnas-Air.lan>
2023-02-19 11:10:41 +0200

    /assets/images/: rearanging some images for mariosofa project page

--- resources/views/frontend/projects/project_mariosofa.blade.php
+++ resources/views/frontend/projects/project_mariosofa.blade.php
@@ -85,39 +85,36 @@
                     <li><i class="fas fa-angle-double-right" style="color:gray"></i><samp> Mailgun - Email service</samp></li>
                     <li><i class="fas fa-angle-double-right" style="color:gray"></i><samp> Sitemap</samp></li>
                     <li><i class="fas fa-angle-double-right" style="color:gray"></i><samp> Google ReCaptcha</samp></li>
-                    <li><i class="fas fa-angle-double-right" style="color:gray"></i><samp> Jetstream - liveware register system for Admin Panel</samp></li>
-                    <li><i class="fas fa-angle-double-right" style="color:gray"></i><samp> Intervention Image Library - PHP Image handling and manipulating library</samp></li>
+                    <li><i class="fas fa-angle-double-right" style="color:gray"></i><samp> Jetstream - Liveware register system for Admin Panel</samp></li>
                     <li><i class="fas fa-angle-double-right" style="color:gray"></i><samp> ToastrJS - Pop-up messages for Admin panel</samp></li>
                     <li><i class="fas fa-angle-double-right" style="color:gray"></i><samp> Summernote - WYSIWYG edito for Admin panel</samp></li>
-                    <li><i class="fas fa-angle-double-right" style="color:gray"></i><samp> ToastrJS - pop-up messages for Admin panel</samp></li>
-                    <li><i class="fas fa-angle-double-right" style="color:gray"></i><samp> Venobox - jQuery Plugin</samp></li>
+                    <li><i class="fas fa-angle-double-right" style="color:gray"></i><samp> Venobox - jQuery Plugin type Lightbox</samp></li>
+                    <li><i class="fas fa-angle-double-right" style="color:gray"></i><samp> Google map integration</samp></li>
                 </ul>
-                <p>Тhe Аdmin panel is equipped with full CRUD functionality for all elements of every page.</p>
-
 
                 <!-- Admin Panel Images -->
                 <div class="d-flex flex-row flex-wrap justify-content-between mt-5">
 
+
                     <!-- First/Left image -->
-                    <div class="col-xl-6 text-center mb-5 mb-lg-0 p-2">
-                        <img class="img-fluid float-xl-right float-sm-none" data-bs-toggle="modal" data-bs-target="#firstModal" src="{{ asset('frontend/assets/images/mariosofa-admin2.png') }}" alt="Mario Sofa & Bed Project Admin Panel">                    
+                    <div class="col-xl-4 text-center mb-5 mb-lg-0 p-2">
+                        <img class="img-fluid float-xl-right float-sm-none" data-bs-toggle="modal" data-bs-target="#firstModal" src="{{ asset('frontend/assets/images/mariosofa-admin1.webp') }}" alt="Mario Sofa & Bed Project Admin Panel">                    
                     </div>
 
-                    <!-- Modal for the first image -->
+                    <!-- Modal for the second image -->
                     <div class="modal fade" id="firstModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                         <div class="modal-dialog modal-xl">
                             <div class="modal-content">
                                 <div class="modal-body">
-                                    <img class="img-fluid float-xl-right float-sm-none" src="{{ asset('frontend/assets/images/mariosofa-admin2.png') }}" alt="Mario Sofa & Bed Project Admin Panel">
+                                    <img class="img-fluid float-xl-right float-sm-none" src="{{ asset('frontend/assets/images/mariosofa-admin1.webp') }}" alt="Mario Sofa & Bed Project Admin Panel">
                                 </div>
                             </div>
                         </div>
                     </div>
 
-
-                    <!-- Second/Right image -->
-                    <div class="col-xl-6 text-center mb-5 mb-lg-0 p-2">
-                        <img class="img-fluid float-xl-right float-sm-none" data-bs-toggle="modal" data-bs-target="#secondModal" src="{{ asset('frontend/assets/images/mariosofa-admin1.png') }}" alt="Mario Sofa & Bed Project Admin Panel">                    
+                    <!-- Second/Center image -->
+                    <div class="col-xl-4 text-center mb-5 mb-lg-0 p-2">
+                        <img class="img-fluid float-xl-right float-sm-none" data-bs-toggle="modal" data-bs-target="#secondModal" src="{{ asset('frontend/assets/images/mariosofa-admin2.webp') }}" alt="Mario Sofa & Bed Project Admin Panel">                    
                     </div>
 
                     <!-- Modal for the second image -->
@@ -125,7 +122,24 @@
                         <div class="modal-dialog modal-xl">
                             <div class="modal-content">
                                 <div class="modal-body">
-                                    <img class="img-fluid float-xl-right float-sm-none" src="{{ asset('frontend/assets/images/mariosofa-admin1.png') }}" alt="Mario Sofa & Bed Project Admin Panel">
+                                    <img class="img-fluid float-xl-right float-sm-none" src="{{ asset('frontend/assets/images/mariosofa-admin2.webp') }}" alt="Mario Sofa & Bed Project Admin Panel">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <!-- Third/Right image -->
+                    <div class="col-xl-4 text-center mb-5 mb-lg-0 p-2">
+                        <img class="img-fluid float-xl-right float-sm-none" data-bs-toggle="modal" data-bs-target="#thirdModal" src="{{ asset('frontend/assets/images/mariosofa-admin3.webp') }}" alt="Mario Sofa & Bed Project Admin Panel">                    
+                    </div>
+
+                    <!-- Modal for the first image -->
+                    <div class="modal fade" id="thirdModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog modal-xl">
+                            <div class="modal-content">
+                                <div class="modal-body">
+                                    <img class="img-fluid float-xl-right float-sm-none" src="{{ asset('frontend/assets/images/mariosofa-admin3.webp') }}" alt="Mario Sofa & Bed Project Admin Panel">
                                 </div>
                             </div>
                         </div>
